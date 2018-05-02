036d0ad7eb9e61f91ed7acc14ea69700c010c920
Tomer Luster <tomerl@webtech-inv.co.il>
2018-05-02 22:37:10 +0300

    test video

--- js/main.js
+++ js/main.js
@@ -30,7 +30,7 @@ $( document ).ready(function() {
         once: true
     });
 
-    var video = document.getElementById('video');
-    video.play();
-    video.loop = true;
+    // var video = document.getElementById('video');
+    // video.play();
+    // video.loop = true;
 });
