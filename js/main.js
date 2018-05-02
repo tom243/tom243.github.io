22d1c3319b0103b4556cfd9995b6ffc453852fdb
Tomer Luster <tomerl@webtech-inv.co.il>
2018-05-02 20:23:16 +0300

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,32 @@
+// var $video  = $('video'),
+//     $window = $(window);
+//
+// $(window).resize(function(){
+//     var height = $window.height();
+//     $video.css('height', height);
+//
+//     var videoWidth = $video.width(),
+//         windowWidth = $window.width(),
+//         marginLeftAdjust =   (windowWidth - videoWidth) / 2;
+//
+//     $video.css({
+//         'height': height,
+//         'marginLeft' : marginLeftAdjust
+//     });
+//
+// }).resize();
+
+
+$( document ).ready(function() {
+    // $(function() {
+    //     $('.lazy').lazy({
+    //         effect: "scrollTop",
+    //         effectTime: 2000,
+    //         threshold: 0
+    //     });
+    // });
+
+    $('.animate').scrolla({
+        once: true
+    });
+});
