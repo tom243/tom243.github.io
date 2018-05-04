b157cb22c17c9c5287e3bf39a88e6cb7fbe89290
Tomer Luster <tomerl@webtech-inv.co.il>
2018-05-04 17:14:54 +0300

    change to youtube iframe

--- js/main.js
+++ js/main.js
@@ -17,20 +17,34 @@
 // }).resize();
 
 
+function sizeTheVideo() {
+    // - 1.78 is the aspect ratio of the video
+// - This will work if your video is 1920 x 1080
+// - To find this value divide the video's native width by the height eg 1920/1080 = 1.78
+    var aspectRatio = 1.78;
+
+    var video = $('#videoWithJs iframe');
+    var videoHeight = video.outerHeight();
+    var newWidth = videoHeight * aspectRatio;
+    var halfNewWidth = newWidth / 2;
+
+    //Define the new width and centrally align the iframe
+    video.css({"width": newWidth + "px", "left": "50%", "margin-left": "-" + halfNewWidth + "px"});
+}
+
 $( document ).ready(function() {
-    // $(function() {
-    //     $('.lazy').lazy({
-    //         effect: "scrollTop",
-    //         effectTime: 2000,
-    //         threshold: 0
-    //     });
-    // });
 
     $('.animate').scrolla({
         once: true
     });
 
-    var video =$("video");
-    video.play();
-    // video.loop = true;
+    // var video =$("video");
+    // video.play();
+
+    // // video.loop = true;
+    // sizeTheVideo();
+    // $(window).resize(function(){
+    //     sizeTheVideo();
+    // });
+
 });
