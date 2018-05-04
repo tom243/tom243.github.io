e36214eddba411d10659fd12217931d663fb18c8
Tomer Luster <tomerl@webtech-inv.co.il>
2018-05-04 17:25:07 +0300

    video index

--- js/main.js
+++ js/main.js
@@ -16,40 +16,10 @@
 //
 // }).resize();
 
-
-function fakeClick(fn) {
-    var $a = $('<a href="#" id="fakeClick"></a>');
-    $a.bind("click", function(e) {
-        e.preventDefault();
-        fn();
-    });
-
-    $("body").append($a);
-
-    var evt,
-        el = $("#fakeClick").get(0);
-
-    if (document.createEvent) {
-        evt = document.createEvent("MouseEvents");
-        if (evt.initMouseEvent) {
-            evt.initMouseEvent("click", true, true, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
-            el.dispatchEvent(evt);
-        }
-    }
-
-    $(el).remove();
-}
-
 $( document ).ready(function() {
 
     $('.animate').scrolla({
         once: true
-    });
-
-    var video = $("video").get(0);
-
-    fakeClick(function() {
-        video.play();
     });
 
 });
